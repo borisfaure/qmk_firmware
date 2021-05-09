2ad67a744efb5217e867c1b0d8677f2fa0166122
Boris Faure <billiob@gmail.com>
2021-05-09 23:10:43 +0200

    keymap: add MISC layer
    + move F* to its own layer

--- keyboards/sofle/keymaps/borisfaure/keymap.c
+++ keyboards/sofle/keymaps/borisfaure/keymap.c
@@ -13,14 +13,16 @@ enum sofle_layers {
     _QWERTY,
     _LOWER,
     _RAISE,
-    _FN,
+    _MISC,
     _NUMBERS,
+    _FN,
 };
 
 enum custom_keycodes {
     KC_LOWER = SAFE_RANGE,
     KC_RAISE,
     KC_FN,
+    KC_MISC,
     KC_AGRV,  // à
     KC_EGRV,  // è
     KC_UGRV,  // ù
@@ -72,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/      /      \      \-----------------------------------------'
- *             |  [  | LGUI | LAlt |LOWER | /Space /        \Enter \  |RAISE | RAlt | RCtrl|   ]  |
+ *             |  [  | LGUI | LAlt |LOWER | /Space /        \Enter \  |RAISE | RAlt |  FN  |   ]  |
  *             `----------------------------------'          '------------------------------------'
  */
 
@@ -81,68 +83,73 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define KC_CTRS RCTL_T(KC_SCLN)
 #define KC_STRS RSFT_T(KC_SLSH)
 #define KC_GUIO RGUI_T(KC_O)
+#define ALT_DOT LALT_T(KC_DOT)
 #define KC_RSE LT(_RAISE, KC_BSPC)
 #define NUM_N LT(_NUMBERS, KC_N)
+#define FN_Y LT(_FN, KC_Y)
 
 /* left hand */
 #define KC_CTLA LCTL_T(KC_A)
 #define KC_LSHZ LSFT_T(KC_Z)
 #define KC_GUIW LGUI_T(KC_W)
+#define ALT_X LALT_T(KC_X)
 #define KC_LWR LT(_LOWER, KC_TAB)
 #define NUM_B LT(_NUMBERS, KC_B)
+#define FN_T LT(_FN, KC_T)
 
 [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,    KC_2,    KC_3,  KC_4,  KC_5,                          KC_6,  KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,    KC_GUIW, KC_E,  KC_R,  KC_T,                          KC_Y,  KC_U, KC_I,    KC_GUIO, KC_P,    KC_BSLS, \
-  KC_LCTRL, KC_CTLA, KC_S,    KC_D,  KC_F,  KC_G,                          KC_H,  KC_J, KC_K,    KC_L,    KC_CTRS, KC_CTRQ, \
-  KC_LSFT,  KC_LSHZ, KC_X,    KC_C,  KC_V,  NUM_B, KC_MUTE,       XXXXXXX, NUM_N,  KC_M, KC_COMM, KC_DOT,  KC_STRS, KC_RSFT, \
+  KC_ESC,   KC_1,    KC_2,    KC_3,  KC_4,  KC_5,                          KC_6,   KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,   KC_Q,    KC_GUIW, KC_E,  KC_R,  FN_T,                          FN_Y,   KC_U, KC_I,    KC_GUIO, KC_P,    KC_BSLS, \
+  KC_LCTRL, KC_CTLA, KC_S,    KC_D,  KC_F,  KC_G,                          KC_H,   KC_J, KC_K,    KC_L,    KC_CTRS, KC_CTRQ, \
+  KC_LSFT,  KC_LSHZ, ALT_X,   KC_C,  KC_V,  NUM_B, KC_MUTE,       XXXXXXX, NUM_N,  KC_M, KC_COMM, ALT_DOT, KC_STRS, KC_RSFT, \
                  KC_LBRC ,KC_LGUI, KC_LALT, KC_LWR, KC_SPC,      KC_ENT, KC_RSE, KC_RALT, KC_FN, KC_RBRC \
 ),
-/* LOWER
+/* LOWER / Symbols
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |   ~  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | Del  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  | F11  | F12  |   é  |   ê  |   è  |                    |      | End  | S-Ins| Home |   -  | PgUp |
+ * |   `  |  !   |   #  |  $   |   (  |   )  |                    |   ^  |  &   | S-Ins|   *  |  ~   | PgUp |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   ∨  |   =  |   -  |   +  |   {  |   }  |-------.    ,-------|   ←  | PgDn | PgUp |   →  |      | PgDn |
+ * |   ∨  |  =   |   -  |  `   |   {  |   }  |-------.    ,-------|   ←  | PgDn | PgUp |   →  |  \   | PgDn |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |   ∨  | Undo | Cut  | Copy | Paste|      |-------|    |-------|      | Menu |      |      |      |   ∨  |
+ * |   ∨  |  @   |   &  |   %  |   [  |   ]  |-------|    |-------|  End | Menu | Home |   '  |  '   |   ∨  |
  * `-----------------------------------------/      /      \      \-----------------------------------------'
- *             |     |      |      |LOWER | /Space /        \ MB1  \  |      |      |      |      |
+ *             |     |      |      |      | / RAlt /        \ MB1  \  | MISC_DEL|      |      |      |
  *             `----------------------------------'          '------------------------------------'
  */
+#define MISCDEL LT(_MISC, KC_DEL)
 [_LOWER] = LAYOUT( \
-  KC_TILD,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,     KC_F9,    KC_F10,  KC_DEL, \
-  KC_GRV,   KC_F11,  KC_F12,  KC_EACU, KC_ECIR, KC_EGRV,                      XXXXXXX, KC_END,  S(KC_INS), KC_HOME,  KC_MINS, KC_PGUP, \
-  _______,  KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,                      KC_LEFT, KC_PGDN, KC_PGUP,   KC_RIGHT, XXXXXXX, KC_PGDN, \
-  _______,  KC_UNDO, KC_CUT, KC_COPY,  KC_PASTE, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, KC_MENU, XXXXXXX,   XXXXXXX,  XXXXXXX, _______, \
-                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,   KC_MB1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX\
+  KC_TILD,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,     KC_F9,    KC_F10,  KC_DEL, \
+  KC_GRV,   KC_EXLM, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN,                     KC_CIRC, KC_AMPR, S(KC_INS), KC_ASTR,  KC_TILD, KC_PGUP, \
+  _______,  KC_EQL,  KC_MINS, KC_GRV,  KC_LCBR, KC_RCBR,                     KC_LEFT, KC_PGDN, KC_PGUP,   KC_RIGHT, KC_BSLS, KC_PGDN, \
+  _______,  KC_AT,   KC_AMPR, KC_PERC, KC_LBRC, KC_RBRC, XXXXXXX,   XXXXXXX, KC_END,  KC_MENU, KC_HOME,   KC_QUOT,  KC_QUOT, _______, \
+                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT,  KC_MB1, MISCDEL, XXXXXXX, XXXXXXX, XXXXXXX\
 ),
 /* RAISE / French
  * ,----------------------------------------.                     ,-----------------------------------------.
  * | Pause|  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | Del  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   ~  | F11  | F12  |   é  |   ê  |   è  |                    |      |  ù   |  î   |  ô   |PrtScr| PgUp |
+ * |   ~  |  !   |  #   |   é  |   ê  |   è  |                    |  "   |  ù   |  î   |  ô   |PrtScr| PgUp |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   ∨  |  à   |   _  |   +  |   {  |   }  |-------.    ,-------|   ←  |  ↓   |   ↑  |   →  |      | PgDn |
+ * |   ∨  |  à   |   _  |   +  |   &  |   |  |-------.    ,-------|   ←  |  ↓   |   ↑  |   →  |  "   | PgDn |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |   ∨  |      |      |   ç  |   [  |   ]  |-------|    |-------|      | Menu |      |      |      |   ∨  |
+ * |   ∨  |      |      |   ç  |   [  |   ]  |-------|    |-------| End  | Menu | Home | Pause|  "   |   ∨  |
  * `-----------------------------------------/      /      \      \-----------------------------------------'
- *             |     |      |      |      | /Space /        \Enter \  |RAISE |      |      |      |
+ *             |     |      |      | MISC | /Space /        \Enter \  |RAISE |      |      |      |
  *             `----------------------------------'          '------------------------------------'
  */
 [_RAISE] = LAYOUT( \
   KC_PAUSE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_DEL, \
-  KC_GRV,   KC_F11,  KC_F12,  KC_EACU, KC_ECIR, KC_EGRV,                     XXXXXXX, KC_UGRV, KC_ICIR, KC_OCIR,  KC_PSCR, KC_PGUP, \
-  _______,  KC_AGRV, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, KC_PGDN, \
-  _______,  XXXXXXX, XXXXXXX, KC_CCED, KC_LBRC, KC_RBRC, XXXXXXX,   XXXXXXX, XXXXXXX, KC_MENU, XXXXXXX, XXXXXXX,  XXXXXXX, _______, \
-                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,   KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX\
+  KC_GRV,   KC_EXLM, KC_HASH, KC_EACU, KC_ECIR, KC_EGRV,                     KC_DQUO, KC_UGRV, KC_ICIR, KC_OCIR,  KC_PSCR, KC_PGUP, \
+  _______,  KC_AGRV, KC_UNDS, KC_PLUS, KC_AMPR, KC_PIPE,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_DQUO, KC_PGDN, \
+  _______,  XXXXXXX, XXXXXXX, KC_CCED, KC_LBRC, KC_RBRC, XXXXXXX,   XXXXXXX,  KC_END, KC_MENU, KC_HOME, KC_PAUS,  KC_DQUO, _______, \
+                      XXXXXXX, XXXXXXX, XXXXXXX, MO(_MISC), KC_SPC,   KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX\
 ),
-/* FN
+/* MISC (Media/Mouse)
  * ,----------------------------------------.                     ,-----------------------------------------.
- * |      | F13  | F14  | F15  | F16  | F17  |                    | F18  | F19  | F20  | F21  | F22  |      |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | F23  | F24  |      |      |      |                    |M_ACL2|      |M_WH_U|      |      |      |
+ * |      |Reset |      |      |      |      |                    |M_ACL2|      |M_WH_U|      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      | VOL_U| MUTE | VOL_D|      |-------.    ,-------|M_ACL1|M_BTN1|M_BTN2|M_BTN3|      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
@@ -151,10 +158,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *             |     |      |      |      | /      /        \      \  |      |      |  FN  |      |
  *             `----------------------------------'          '------------------------------------'
  */
-  [_FN] = LAYOUT( \
-  XXXXXXX, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,                      KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  XXXXXXX, \
-  XXXXXXX, KC_F23,  KC_F24,  XXXXXXX, XXXXXXX, XXXXXXX,                     KC_ACL2, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,                     KC_ACL1, KC_MB1,  KC_MB3,  KC_MB2, XXXXXXX, XXXXXXX, \
+  [_MISC] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX,   RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_ACL2, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,                     KC_ACL1,  KC_MB1,  KC_MB3,  KC_MB2, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,   XXXXXXX, KC_ACL0, XXXXXXX, KC_WH_D, XXXXXXX, XXXXXXX, XXXXXXX, \
                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, KC_FN, XXXXXXX\
   ),
@@ -162,9 +169,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------------.                     ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  0   |  1   |   2  |  3   |   +  |                    |   0  |  1   |  2   |   3  |   +  |      |
+ * |      |  /   |  4   |   5  |  6   |   +  |                    |   /  |  4   |  5   |   6  |   +  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  /   |  4   |   5  |  6   |   -  |-------.    ,-------|   /  |  4   |  5   |   6  |   -  |      |
+ * |      |  0   |  1   |   2  |  3   |   -  |-------.    ,-------|   0  |  1   |  2   |   3  |   -  |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |  *   |  7   |   8  |  9   |      |-------|    |-------|      |  7   |  8   |   9  |   =  |      |
  * `-----------------------------------------/      /      \      \-----------------------------------------'
@@ -173,10 +180,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_NUMBERS] = LAYOUT( \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX,    KC_0,    KC_1,    KC_2,    KC_3,    KC_0,                        KC_0,    KC_1,    KC_2,    KC_3, KC_PLUS, XXXXXXX, \
-  XXXXXXX, KC_STRS,    KC_4,    KC_5,    KC_6, KC_PLUS,                     KC_STRS,    KC_4,    KC_5,    KC_6, KC_MINS, XXXXXXX, \
-  XXXXXXX, KC_ASTR,    KC_7,    KC_8,    KC_9, KC_MINS, XXXXXXX,   XXXXXXX, XXXXXXX,    KC_7,    KC_8,    KC_9,  KC_EQL, XXXXXXX, \
+  XXXXXXX, KC_STRS,    KC_4,    KC_5,    KC_6, KC_PLUS,                     KC_STRS,    KC_4,    KC_5,    KC_6, KC_PLUS, XXXXXXX, \
+  XXXXXXX,    KC_0,    KC_1,    KC_2,    KC_3, KC_MINS,                        KC_0,    KC_1,    KC_2,    KC_3, KC_MINS, XXXXXXX, \
+  XXXXXXX, KC_ASTR,    KC_7,    KC_8,    KC_9, _______, XXXXXXX,   XXXXXXX, _______,    KC_7,    KC_8,    KC_9,  KC_EQL, XXXXXXX, \
                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX\
+  ),
+/*
+ * FN
+ * ,----------------------------------------.                     ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |  F1  | F2   | F3   | F4   |      |                    |      |  F1  | F2   | F3   | F4   |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |  F5  | F6   | F7   | F8   |      |-------.    ,-------|      |  F5  | F6   | F7   | F8   |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |  F9  | F10  | F11  | F12  |      |-------|    |-------|      |  F9  | F10  | F11  | F12  |      |
+ * `-----------------------------------------/      /      \      \-----------------------------------------'
+ *             |     |      |      |      | /      /        \      \  |      |      |      |      |
+ *             `----------------------------------'          '------------------------------------'
+ */
+  [_FN] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                     XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX, \
+  XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                     XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX, \
+  XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX, \
+                    XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,   _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
   )
 };
 // clang-format on
@@ -203,11 +231,14 @@ void pointing_device_task() {
         case _LOWER:
             trackball_set_rgbw(0, 153, 95, 0);
             break;
-        case _FN:
+        case _MISC:
             trackball_set_rgbw(153, 0, 110, 0);
             break;
         case _NUMBERS:
             trackball_set_rgbw(255, 0, 0, 0);
+            break;
+        case _FN:
+            trackball_set_rgbw(153, 0, 110, 0);
             break;
         default:
             trackball_set_timed_rgbw(0, 0, 0, 80);
@@ -242,11 +273,14 @@ static void print_status_narrow(void) {
         case _LOWER:
             oled_write_P(PSTR("Lower"), false);
             break;
-        case _FN:
-            oled_write_P(PSTR("Function\n"), false);
+        case _MISC:
+            oled_write_P(PSTR("Misc\n"), false);
             break;
         case _NUMBERS:
-            oled_write_P(PSTR("Numbers\n"), false);
+            oled_write_P(PSTR("Num\n"), false);
+            break;
+        case _FN:
+            oled_write_P(PSTR("FN\n"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
